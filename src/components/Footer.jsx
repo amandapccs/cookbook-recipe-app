8beb4c531342f8af593e037376228ec3566e2f4b
Luísa Vilas Boas dos Santos <luisa13santos@outlook.com>
2022-06-21 17:16:05 -0300

    [Footer] Requisito 20

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,39 +8,36 @@ import mealIcon from '../images/mealIcon.svg';
 export default function Footer() {
   return (
     <footer
+      className="footerConteiner"
       data-testid="footer"
     >
-      <div
-        className="footerConteiner"
+      <Link
+        to="/drinks"
       >
-        <Link
-          to="/drinks"
-        >
-          <img
-            src={ drinkIcon }
-            alt="drinkIcon"
-            data-testid="drinks-bottom-btn"
-          />
-        </Link>
-        <Link
-          to="/explore"
-        >
-          <img
-            src={ exploreIcon }
-            alt="exploreIcon"
-            data-testid="explore-bottom-btn"
-          />
-        </Link>
-        <Link
-          to="/foods"
-        >
-          <img
-            src={ mealIcon }
-            alt="mealIcon"
-            data-testid="food-bottom-btn"
-          />
-        </Link>
-      </div>
+        <img
+          src={ drinkIcon }
+          alt="drinkIcon"
+          data-testid="drinks-bottom-btn"
+        />
+      </Link>
+      <Link
+        to="/explore"
+      >
+        <img
+          src={ exploreIcon }
+          alt="exploreIcon"
+          data-testid="explore-bottom-btn"
+        />
+      </Link>
+      <Link
+        to="/foods"
+      >
+        <img
+          src={ mealIcon }
+          alt="mealIcon"
+          data-testid="food-bottom-btn"
+        />
+      </Link>
     </footer>
   );
 }
