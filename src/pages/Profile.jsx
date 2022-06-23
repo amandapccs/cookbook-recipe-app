c275b4bc28c5a1da741191f91b61a544da1b0a3f
RicardoOpus <ricardoopus125@gmail.com>
2022-06-23 18:15:31 -0300

    Página de Perfil - Envia requisitos 82, 83 e 84

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,11 +5,37 @@ import { Context } from '../context/Provider';
 
 export default function Profile() {
   const { setShowSearchButton } = useContext(Context);
+  const readUser = JSON.parse(localStorage.getItem('user'));
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => setShowSearchButton(false), []);
   return (
     <div>
       <Header title="Profile" />
+      <main>
+        <div data-testid="profile-email">
+          { readUser.email }
+        </div>
+        <form>
+          <button
+            data-testid="profile-done-btn"
+            type="button"
+          >
+            Done Recipes
+          </button>
+          <button
+            data-testid="profile-favorite-btn"
+            type="button"
+          >
+            Favorite Recipes
+          </button>
+          <button
+            data-testid="profile-logout-btn"
+            type="button"
+          >
+            Logout
+          </button>
+        </form>
+      </main>
       <Footer />
     </div>
   );
