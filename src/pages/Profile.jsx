c2e3f6297cb0bc60872a236065e4ccdc9174b1bf
RicardoOpus <ricardoopus125@gmail.com>
2022-06-23 18:52:52 -0300

    Página de Perfil - Envia requisitos 85, 86 e 87

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Context } from '../context/Provider';
@@ -8,6 +9,15 @@ export default function Profile() {
   const readUser = JSON.parse(localStorage.getItem('user'));
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => setShowSearchButton(false), []);
+  const clearStorage = () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('mealsToken');
+    localStorage.removeItem('cocktailsToken');
+    localStorage.removeItem('doneRecipes');
+    localStorage.removeItem('favoriteRecipes');
+    localStorage.removeItem('inProgressRecipes');
+  };
+
   return (
     <div>
       <Header title="Profile" />
@@ -16,24 +26,31 @@ export default function Profile() {
           { readUser.email }
         </div>
         <form>
-          <button
-            data-testid="profile-done-btn"
-            type="button"
-          >
-            Done Recipes
-          </button>
-          <button
-            data-testid="profile-favorite-btn"
-            type="button"
-          >
-            Favorite Recipes
-          </button>
-          <button
-            data-testid="profile-logout-btn"
-            type="button"
-          >
-            Logout
-          </button>
+          <Link to="/done-recipes">
+            <button
+              data-testid="profile-done-btn"
+              type="button"
+            >
+              Done Recipes
+            </button>
+          </Link>
+          <Link to="/favorite-recipes">
+            <button
+              data-testid="profile-favorite-btn"
+              type="button"
+            >
+              Favorite Recipes
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              onClick={ clearStorage }
+              data-testid="profile-logout-btn"
+              type="button"
+            >
+              Logout
+            </button>
+          </Link>
         </form>
       </main>
       <Footer />
