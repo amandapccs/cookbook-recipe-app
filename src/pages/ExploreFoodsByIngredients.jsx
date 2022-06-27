70d429930df4144d985374ce038d77bb47cd5138
Gabriel Flach <g.flachh@outlook.com>
2022-06-27 17:37:26 -0300

    Tentativa de criar um botão que leve para a pagina Foods, ja filtrata com o ingrediente

--- src/pages/ExploreFoodsByIngredients.jsx
+++ src/pages/ExploreFoodsByIngredients.jsx
@@ -1,13 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Context } from '../context/Provider';
+import { Context, Context as RecipeContext } from '../context/Provider';
 import Card from '../components/RecomendationCard';
 
 export default function ExploreFoodsByIngredients() {
   const { setShowSearchButton } = useContext(Context);
+  const history = useHistory();
 
   const [ingredients, setIngredients] = useState([]);
+
+  const {
+    setFetchedFoodOrDrink,
+  } = useContext(RecipeContext);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => setShowSearchButton(false), []);
@@ -16,11 +22,19 @@ export default function ExploreFoodsByIngredients() {
     async function fetchFoodByIngredients() {
       const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
       const resolve = await response.json();
-      console.log(resolve.meals);
+      // console.log(resolve.meals);
       setIngredients(resolve.meals);
     }
     fetchFoodByIngredients();
   }, []);
+
+  const handleClick = async (name) => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${name}`);
+    const resolve = await response.json();
+    console.log(resolve.meals);
+    setFetchedFoodOrDrink(resolve.meals);
+    history.push('/foods');
+  };
 
   const MAX_INGREDIENTS = 12;
 
@@ -30,16 +44,20 @@ export default function ExploreFoodsByIngredients() {
       <div>
         {ingredients.slice(0, MAX_INGREDIENTS)
           .map(({ idIngredient, strIngredient }, index) => (
-            <Card
+            <button
+              type="button"
+              onClick={ () => handleClick(strIngredient) }
               key={ index }
-              src={ `https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png` }
-              name={ strIngredient }
-              testDiv={ `${index}-ingredient-card` }
-              testTitle={ `${index}-card-name` }
-              testImg={ `${index}-card-img` }
-              id={ idIngredient }
-              path="drinks"
-            />
+            >
+              <Card
+                src={ `https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png` }
+                name={ strIngredient }
+                testDiv={ `${index}-ingredient-card` }
+                testTitle={ `${index}-card-name` }
+                testImg={ `${index}-card-img` }
+                id={ idIngredient }
+              />
+            </button>
           ))}
       </div>
       <Footer />
