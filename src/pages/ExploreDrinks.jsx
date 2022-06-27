2e43078afd3ec7c826cd84dda676859a07087308
Luísa Vilas Boas dos Santos <luisa13santos@outlook.com>
2022-06-27 15:50:54 -0300

    [Explore] - Implementa a página de Explore, ExploreFoods e ExploreDrinks

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Context } from '../context/Provider';
@@ -7,9 +8,40 @@ export default function ExploreDrinks() {
   const { setShowSearchButton } = useContext(Context);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => setShowSearchButton(false), []);
+
+  const [randomDrinkId, setRandomDrinkId] = useState('11011');
+
+  useEffect(() => {
+    const fetchFoodRandom = async () => {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+      const data = await response.json();
+      const drinkDetailsData = data.drinks[0].idDrink;
+      setRandomDrinkId(drinkDetailsData);
+      console.log(drinkDetailsData);
+    };
+    fetchFoodRandom();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const history = useHistory();
+
   return (
     <div>
       <Header title="Explore Drinks" />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/drinks/ingredients') }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push(`/drinks/${randomDrinkId}`) }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Context } from '../context/Provider';
@@ -7,9 +8,47 @@ export default function ExploreFoods() {
   const { setShowSearchButton } = useContext(Context);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => setShowSearchButton(false), []);
+
+  const [randomFoodId, setRandomFoodId] = useState('53024');
+
+  useEffect(() => {
+    const fetchFoodRandom = async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+      const data = await response.json();
+      const foodDetailsData = data.meals[0].idMeal;
+      setRandomFoodId(foodDetailsData);
+      console.log(foodDetailsData);
+    };
+    fetchFoodRandom();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const history = useHistory();
+
   return (
     <div>
       <Header title="Explore Foods" />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => history.push('/explore/foods/nationalities') }
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push(`/foods/${randomFoodId}`) }
+      >
+        Surprise me!
+      </button>
       <Footer />
     </div>
   );
